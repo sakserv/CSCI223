c2218365de594f4dd1777195daf745e4001b5b4e
Shane Kumpf <sakserv@sakserv.org>
2012-09-08 10:01:25 -0500

    Assignment 3 complete

--- HTML_responsive/WebContent/js/form.js
+++ HTML_responsive/WebContent/js/form.js
@@ -5,6 +5,8 @@ function submitForm(form){
 	var name = document.comment_form.name.value;
 	var email = document.comment_form.email.value;
 	var comments = document.comment_form.comments.value;
+	var age = document.comment_form.age.value;
+	var ageDiv = document.getElementById("age");
 	
 	if (name == '') {
 		error.innerHTML = "Name is a required field.";
@@ -15,10 +17,30 @@ function submitForm(form){
 	} else if (comments == '') {
 		error.innerHTML = "Comments is a required field.";
 		error.style.display = "block";
-	} else {	
-	    formdiv = document.getElementById("formwrap");
-	    formdiv.style.textAlign = "center";
-	    document.getElementById("submit").style.display = "none";
-	    formdiv.innerHTML = "Thanks for the feedback " + name + "!!";
+	} else if (age == '' && ageDiv.style.display == "block"){
+		error.innerHTML = "You indicated you are over 21. Age is required";
+		error.style.display = "block";
+	} else {
+		document.comment_form.action = 'results.html';
+		document.comment_form.submit();
+	}
+}
+
+function showAgeField(){
+	var ageField = document.getElementById("age");
+	ageField.style.display = "block";
+}
+
+function displayResults() {
+	
+	queryString = window.location.search;
+	document.getElementById("formResp").innerHTML = "Thanks for your submission";
+	document.getElementById("formName").innerHTML = "Name: " + queryString.substring(1).split("&")[0].split("=")[1];
+	document.getElementById("formEmail").innerHTML = "Email: " + queryString.substring(1).split("&")[1].split("=")[1];
+	document.getElementById("formComments").innerHTML = "Comments: " + queryString.substring(1).split("&")[2].split("=")[1];
+	
+	var ageVal = queryString.substring(1).split("&")[4].split("=")[1];
+	if (ageVal != '') {
+		document.getElementById("formAge").innerHTML = "Age: " + ageVal;
 	}
 }
